ddc66f67d82ec9bf653bb46437f5695d6bf6f635
tamakiii <tamakiii@users.noreply.github.com>
2023-07-29 01:00:35 +0900

    impl without clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,55 @@
+// use queue_file::QueueFile;
+use std::env::args;
+
+struct Args {
+    file: String,
+}
+
+struct ParseViolation {
+    root: String,
+    message: String,
+}
+
+struct ParseError {
+    violations: Vec<ParseViolation>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    match parse_args() {
+        Ok(args) => println!("{}", args.file),
+        Err(errors) => {
+            for violation in errors.violations {
+                println!("Error: {} at `{}`", violation.message, violation.root);
+            }
+            println!("");
+            println!("USAGE:");
+            println!("  hello-queue-file <file>");
+        }
+    };
+}
+
+fn parse_args() -> Result<Args, ParseError> {
+    struct Parsed {
+        file: Option<String>,
+    }
+    let file = args().nth(1).or(None);
+    let parsed = Parsed {
+        file: file.or(None),
+    };
+
+    let mut violations = vec![];
+    if parsed.file == None {
+        violations.push(ParseViolation {
+            root: "file".to_string(),
+            message: format!("No {} specified", "file"),
+        });
+    }
+
+    if violations.len() > 0 {
+        return Err(ParseError { violations });
+    }
+
+    Ok(Args {
+        file: parsed.file.unwrap(),
+    })
 }
