176d19f40598f305619922f1c83d047592f641e2
tamakiii <tamakiii@users.noreply.github.com>
2023-07-29 11:56:03 +0900

    using clap

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,15 @@
 // use queue_file::QueueFile;
-use std::env::args;
+use clap::Parser;
 
+#[derive(Parser)]
 struct Args {
+    /// The file to put queues
     file: String,
 }
 
-struct ParseViolation {
-    root: String,
-    message: String,
-}
-
-struct ParseError {
-    violations: Vec<ParseViolation>,
-}
-
 fn main() {
-    match parse_args() {
-        Ok(args) => {
-            println!("{}", args.file)
-        }
-        Err(errors) => {
-            for violation in errors.violations {
-                println!("Error: {} at `{}`", violation.message, violation.root);
-            }
-            println!("");
-            println!("USAGE:");
-            println!("  hello-queue-file <file>");
-        }
-    };
-}
-
-fn parse_args() -> Result<Args, ParseError> {
-    struct Parsed {
-        file: Option<String>,
-    }
-    let file = args().nth(1).or(None);
-    let parsed = Parsed {
-        file: file.or(None),
-    };
-
-    let mut violations = vec![];
-    if parsed.file == None {
-        violations.push(ParseViolation {
-            root: "file".to_string(),
-            message: format!("No {} specified", "file"),
-        });
-    }
-
-    if violations.len() > 0 {
-        return Err(ParseError { violations });
-    }
-
-    Ok(Args {
-        file: parsed.file.unwrap(),
-    })
+    let args = Args::parse();
+    println!("{}", args.file)
 }
 
 #[cfg(test)]
